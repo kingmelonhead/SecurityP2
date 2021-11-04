87d2cfdfe8bb739451172abffe9670f0b6d74b91
John Hackstadt <jhackstadt50@gmail.com>
2021-11-04 01:34:58 -0500

    started task3

--- src/Task3.java
+++ src/Task3.java
@@ -1,15 +1,16 @@
+import java.io.*;
+import java.util.Objects;
 import java.util.Scanner;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.nio.charset.StandardCharsets;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Task3 {
 
     static Scanner text_scanner;
 
-    public static void main(String args[]){
+    public static void main(String[] args) throws IOException {
 
         text_scanner = new Scanner(System.in);
 
@@ -21,17 +22,20 @@ public class Task3 {
         max_pass = get_max_pass();
 
         //get whether or not the user wants to crack the hashed file or the salted file
-        System.out.println("Would you like to crack a password from the hashed file or the salted file\n" +
-                "1) Hashed\n2) Salted\n");
+        System.out.println("""
+                Would you like to crack a password from the hashed file or the salted file
+                1) Hashed
+                2) Salted
+                """);
 
         //get the valid option from the user
         while (!valid_input){
             temp = text_scanner.nextLine();
-            if (temp == "1"){
+            if (Objects.equals(temp, "1")){
                 op = 1;
                 valid_input = true;
             }
-            else if (temp == "2"){
+            else if (Objects.equals(temp, "2")){
                 op = 2;
                 valid_input = true;
             }
@@ -47,24 +51,82 @@ public class Task3 {
 
     }
 
-    private static void crack_hash(int max){
+    private static void crack_hash(int max) throws IOException {
+        String test_pass = "";
+        BufferedReader reader;
+        FileReader in_file = new FileReader("hashed.txt");
+        int random_line = ThreadLocalRandom.current().nextInt(0, 99 + 1);
+        String line;
+        int line_no = 0;
+        reader = new BufferedReader(in_file);
+        while ((line = reader.readLine()) != null){
+            if (line_no == random_line){
+                break;
+            }
+            else {
+                line_no++;
+            }
+        }
+        if (line == null){
+            System.out.println("Error line is null, exiting early...");
+            System.exit(0);
+        }
+        String[] split_string = line.split(" ");
+        String username = split_string[0];
+        String pass_from_file = split_string[1];
+
         //start brute forcing passwords up to size max
+        while (true){
 
-        //hash it
+            //hash it
 
-        //compare
+            //compare
+
+
+            if (test_pass.equals(pass_from_file)){
+                break;
+            }
+        }
     }
 
-    private static void crack_salt(int max){
-        //take the salt from the line in file
+    private static void crack_salt(int max) throws IOException {
+        String test_pass = "";
+        BufferedReader reader;
+        FileReader in_file = new FileReader("salted.txt");
+        int random_line = ThreadLocalRandom.current().nextInt(0, 99 + 1);
+        String line;
+        int line_no = 0;
+        reader = new BufferedReader(in_file);
+        while ((line = reader.readLine()) != null){
+            if (line_no == random_line){
+                break;
+            }
+            else {
+                line_no++;
+            }
+        }
+        if (line == null){
+            System.out.println("Error line is null, exiting early...");
+            System.exit(0);
+        }
+        String[] split_string = line.split(" ");
+        String username = split_string[0];
+        char salt = split_string[1].charAt(0);
+        String pass_from_file = split_string[2];
+
 
         //start brute forcing passwords up to size max
+        while (true){
+            //salt it
 
-        //salt it
+            //hash it
 
-        //hash it
+            //compare
 
-        //compare
+            if (test_pass.equals(pass_from_file)){
+                break;
+            }
+        }
     }
 
     private static int get_max_pass(){
